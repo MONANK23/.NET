4f501d90d695fb488ea25244240a2f3e0c804935
Monank Kalavadiya <140997714+MONANK23@users.noreply.github.com>
2025-06-24 08:59:22 +0530

    Add files via upload

---
+++ LAB 5/lab5/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+//class Program
+//{
+
+//    public static void Main(string[] args)
+//    {
+//    ArrayList StudentName = new ArrayList();
+//        StudentName.Add("a");
+//        StudentName.Add("b");
+//        StudentName.Add("b");
+//        StudentName.Add("c");
+//        StudentName.Add("d");
+//        StudentName.Remove("ABC");
+//        StudentName.RemoveRange(1,2);
+//        StudentName.Clear();
+//    }
+//}
+
+
+//class Program
+//{
+//    public static void main(string[] args)
+//    {
+//        List<dynamic> l = new List<dynamic>();
+//        l.Add("Hello");
+//        l.Add("Hello");
+//        l.Add("Hello2");
+//        l.Add("Hello3");
+//        l.Add("Hello4");
+//        l.Add("Hello5");
+//        l.Remove("World");
+//        l.RemoveRange(0,1);
+//        l.Clear();
+//    }
+//}
+
+
+//class Program
+//{
+//    public static void main(string[] args)
+//    {
+//        Stack<int> s = new Stack<int>();
+//        s.Push(10);
+//        s.Push(11);
+//        s.Push(12);
+//        s.Push(13);
+//        s.Push(14);
+//        s.Push(15);
+//        s.Pop();
+//        s.Peek();
+//        s.Contains(10);
+//        s.Clear();
+//    }
+//}
+
+
+//class Program
+//{
+//    public static void main(string[] args)
+//    {
+//        Queue<int> q = new Queue<int>();
+//        q.Enqueue(1);
+//        q.Enqueue(2);
+//        q.Enqueue(3);
+//        q.Enqueue(4);
+//        q.Enqueue(5);
+//        q.Dequeue();
+//        q.Peek();
+//        q.Contains(1);
+//        q.Clear();
+//    }
+//}
+
+//class Program
+//{
+//    public static void main(string[] args)
+//    {
+//        Dictionary<int, String> d = new Dictionary<int, String>();
+//        d.Add(1, "Hello1");
+//        d.Add(2, "Hello2");
+//        d.Add(3, "Hello3");
+//        d.Add(4, "Hello4");
+//        d.Add(5, "Hello5");
+//        d.Add(6, "Hello6");
+//        d.Remove(2);
+//        d.ContainsKey(2);
+//        d.ContainsValue("Hello1");
+//        d.Clear();
+//    }
+//}
+
+
+//class Program
+//{
+//    public static void main(string[] args)
+//    {
+//        Hashtable ht = new Hashtable();
+//        ht.Add(1, "Hello1");
+//        ht.Add(2, "Hello2");
+//        ht.Add(3, "Hello3");
+//        ht.Add(4, "Hello4");
+//        ht.Add(5, "Hello5");
+//        ht.Remove(2);
+//        ht.ContainsKey(2);
+//        ht.ContainsValue("Hello2");
+//        ht.Clear();
+//    }
+//}
